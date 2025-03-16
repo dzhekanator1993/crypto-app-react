92c5a309fc5a4290d43e83b53452f658152a372c
dzhekanator1993 <dzhekanator1993@gmail.com>
2025-03-16 20:07:15 +0200

    init app

---
+++ CryptoPage/src/api.js
@@ -0,0 +1,16 @@
+import { cryptoAssets, cryptoData } from "./data";
+
+export function fakeFetchCrypto() {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve(cryptoData)
+    }, 100)
+  })
+}
+export function fakeFetchAssets() {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve(cryptoAssets)
+    }, 200)
+  })
+}
---
+++ CryptoPage/src/components/layout/AppLayout.jsx
@@ -0,0 +1,23 @@
+import { Layout, Spin } from 'antd';
+import AppContent from "./AppContent.jsx";
+import AppHeader from "./AppHeader.jsx";
+import AppSider from "./AppSider.jsx";
+import { useContext } from 'react';
+import CryptoContext from "./../../context/crypto-context.jsx";
+
+export default function AppLayout() {
+  const {loading} = useContext(CryptoContext)  
+
+  if(loading){
+    return <Spin fullscreen/>
+  }
+  return (
+      <Layout>
+        <AppHeader />
+        <Layout>
+          <AppSider />
+          <AppContent />
+        </Layout>
+      </Layout>
+  )
+}
